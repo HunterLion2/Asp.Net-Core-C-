dc68158a8d0e7c1688046a2c6ed72ea24431c72e
Çınar Özgürgil <cozgurgil@gmail.com>
2025-03-06 19:44:39 +0300

    String

---
+++ Strings.cs
@@ -0,0 +1,20 @@
+
+
+class String {
+
+    
+    string name = Console.ReadLine();
+    string surname = "Özgürgil";
+    int age = 18;
+
+    // string str = "My name is" + name + "" + surname + "I'm " + age + " years old";
+
+    // Buradaki 0 1 2 değerleri string.Format değerinin sonun da verdiğim değerlerdir bunun sıralaması bu şekilde ya da yukarıdaki gibi de yapılabilir.
+
+    // string str = string.Format("My name is {0} {1} and I'm {2} years old", name,surname,age);
+
+    // Bu şekilde de yapılabilir aynı şeydir.
+    // string str = "$ My name is {name} {surname} and I'm {age} years old.";
+
+}
+
--- obj/Debug/net9.0/Asp.Net-Core C#.AssemblyInfo.cs
+++ obj/Debug/net9.0/Asp.Net-Core C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asp.Net-Core C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6911eb856216e684e947bdbcaccfd109813e361b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdecad802e0abe7fd37096b38c13ed3823da5871")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asp.Net-Core C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asp.Net-Core C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
