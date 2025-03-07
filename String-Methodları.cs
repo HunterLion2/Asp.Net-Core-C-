420cd244adaa9ba92096522cb3875f651f974553
Çınar Özgürgil <cozgurgil@gmail.com>
2025-03-07 12:35:13 +0300

    String-Methodları-Bitti

--- String-Methodları.cs
+++ String-Methodları.cs
@@ -5,6 +5,24 @@ class StringMethod {
 
     static void Main(string[] args) {
         string msg = "Hello There. My name is Çınar Özgürgil.";
-    }
 
+        // Console.WriteLine(msg.Length); length yazılan değerin uzunluğunu belirtir.
+        // Console.WriteLine(msg.ToLower()); ToLower() bütün cümleyi küçük harf yapar.
+        // Console.WriteLine(msg.ToUpper()); ToUpper() bütün cümleyi büyük harf yapar.
+        // Console.WriteLine(msg.Trim()); Trim() değeri cümle içerisinde ki boşlukları siler.
+        // Console.WriteLine(msg.Split()[0]); Her boşluktan önceki cümleyi bir eleman olarak ayırır ve [] içerisinde de onu çağırmamızı sağlar.
+        // Console.WriteLine(msg.StartsWith("Hello")); StartsWith("Hello") değeri msg değeri Hello ile mi başlıyor diye bir soru sormuş oluruz sonucunda da true-false değer alırız.
+        // Console.WriteLine(msg.EndsWith("Hello")); EndsWith("Hello") değeri ise msg değeri Hello ile mi bitiyor sorusu sormuş oluruz ve true-false değer almış oluruz.
+        // Console.WriteLine(msg.Contains("Here")); Contains değeri msg değeri içerisinde içine girilen değer varmı yokmu diye bakar.
+        // Console.WriteLine(msg.IndexOf("name")); Bu değer ise name değerinin hangi index değerinde başladığını getirir.
+        // Console.WriteLine(msg.Substring(5)); Substring değeri 5. index değerinden itibaren değeri getirir sona kadar.
+        // Console.WriteLine(msg.Substring(5,10)); Burada ise 5. değerden 10. değere kadar ki değeri getirir.
+
+        // int index = msg.IndexOf("name");
+        // Console.WriteLine(msg.Substring(index)); Burada yukarıda name değerinin index değerini buldurduk sonrasında Substring değerine de bu değerden itibaren getir dedik
+        // Console.WriteLine(msg.Replace("","-").Replace(".","")); Replace değeri içine girilen ilk değeri ikinci girilene çevirir.
+        // Console.WriteLine(msg.Insert(0, "...")); Burada 0. değere ... değerini ekler.} 
+        // Console.WriteLine(msg.Remove(5)); 5. indexden itibaren siler
+        // Console.WriteLine(msg.Remove(5,10)); 5. indexden 10. indexe kadar siler
+    }
 }
--- obj/Debug/net9.0/Asp.Net-Core C#.AssemblyInfo.cs
+++ obj/Debug/net9.0/Asp.Net-Core C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asp.Net-Core C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc68158a8d0e7c1688046a2c6ed72ea24431c72e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+946f1cad47cfe98365aa79f40781eaba2a5c3008")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asp.Net-Core C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asp.Net-Core C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
