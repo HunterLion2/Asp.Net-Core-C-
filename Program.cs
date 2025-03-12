09771d06e331c0d8635613cf96569dc0cc3e958a
Çınar Özgürgil <cozgurgil@gmail.com>
2025-03-12 16:06:48 +0300

    Uygulamalar

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Globalization;
+﻿
 
 namespace ConsoleApp
 {
@@ -15,42 +14,42 @@ namespace ConsoleApp
             // da öbürlerinden daha büyüktür bu gözetilerek yazılmalıdır.
 
             // Tam Sayı (byte,short,int,long)
-            int sayi = 10;
-            int sayi2 = 20;
+            // int sayi = 10;
+            // int sayi2 = 20;
 
-            long sayi3 = 50;
-            short sayi4 = 5;
-            byte sayi5 = 10;
+            // long sayi3 = 50;
+            // short sayi4 = 5;
+            // byte sayi5 = 10;
 
             // Ondalıklı Sayı
-            float a = 10.5f; // C# da float bir değer yazarken sonuna "f" konur.
-            double b = 20.6;
-            decimal c = 100.56m; // C# da decimal bir değer yazarken sonuna "m" konur.
+            // float a = 10.5f; // C# da float bir değer yazarken sonuna "f" konur.
+            // double b = 20.6;
+            // decimal c = 100.56m; // C# da decimal bir değer yazarken sonuna "m" konur.
 
             // Karakter
             // ( String değerde söz dizileri vardır, Char'da ise tek harf girilir ve tek tırnak ile girilir. )
-            string name = "Çınar Özgürgil";
-            char ch = 'b'; // Burada bu şekilde tek tırnak kullanılır
+            // string name = "Çınar Özgürgil";
+            // char ch = 'b'; // Burada bu şekilde tek tırnak kullanılır
 
             // Boolean
-            bool isRetired = true;
-            bool isActive = false;
+            // bool isRetired = true;
+            // bool isActive = false;
 
 
-            Console.WriteLine(sayi);
-            System.Console.WriteLine(sayi2);
+            // Console.WriteLine(sayi);
+            // System.Console.WriteLine(sayi2);
 
             // UYGULAMA
 
-            byte kilo = 78;
-            string plaka = "35 AC 5553";
-            long km = 128500;
-            int id = 123123;
-            bool isActiva = false;
-            int Maaş = 100500;
-            string ad = "Çınar";
-            string soyad = "Özgürgil";
-            char şubekodu = 'A';
+            // byte kilo = 78;
+            // string plaka = "35 AC 5553";
+            // long km = 128500;
+            // int id = 123123;
+            // bool isActiva = false;
+            // int Maaş = 100500;
+            // string ad = "Çınar";
+            // string soyad = "Özgürgil";
+            // char şubekodu = 'A';
 
             //                                  --------------------------- Veri Tipi Dönüşümü ------------------------------------
 
@@ -60,11 +59,11 @@ namespace ConsoleApp
 
             // implicit casting (automatically): smaller type to larger [Yani aslında bu 32-bit'i 64-bit'e çevirmedir].
 
-            int ö = 10;
-            long ü = ö;
+            // int ö = 10;
+            // long ü = ö;
 
-            float e = 10.5f;
-            double f = e;
+            // float e = 10.5f;
+            // double f = e;
 
             // explicit casting (manually): larger type to smaller [ Bu da 64-bit'i 32-bit'e dönüştürmedir fakat bu atama yapılırken aşşağıda olduğu (int)c ile c int'e çevrilir.]
             // Fakat bu şekilde işlem yaparken veri kaybı olduğu için burada çıkacak sonuç 32-bit'in karşılıyabildiği max değerdir.
@@ -72,95 +71,95 @@ namespace ConsoleApp
             // long c = 10;
             // int d = (int)c;
 
-            double g = 10;
-            float h = (float)g;
+            // double g = 10;
+            // float h = (float)g;
 
-            double k = 10.5;
-            int l = (int)k;
+            // double k = 10.5;
+            // int l = (int)k;
 
             // int to string
 
-            int x = 10;
-            string z = x.ToString(); // Stringe dönüştürme bu şekilde olur.
+            // int x = 10;
+            // string z = x.ToString(); // Stringe dönüştürme bu şekilde olur.
 
-            Console.WriteLine(l);
+            // Console.WriteLine(l);
 
 
-            Console.Write("1.sayı: ");
-            int sayi1 = int.Parse(Console.ReadLine()); // Buradaki int.Parse() olayı ise Console.ReadLine()'dan gelen string ifadeyi int değerine döndürür.
+            // Console.Write("1.sayı: ");
+            // int sayi1 = int.Parse(Console.ReadLine()); // Buradaki int.Parse() olayı ise Console.ReadLine()'dan gelen string ifadeyi int değerine döndürür.
 
-            Console.Write("2.sayı: ");
-            // int sayi2 = int.Parse(Console.ReadLine());
+            // Console.Write("2.sayı: ");
+            // // int sayi2 = int.Parse(Console.ReadLine());
 
-            int toplam = sayi2 + sayi1;
-            Console.WriteLine($"Girilen Toplam Değer: {toplam}"); // $ işaretini kullanmazsal o zaman içerisine int değerini yazamazdık.
+            // int toplam = sayi2 + sayi1;
+            // Console.WriteLine($"Girilen Toplam Değer: {toplam}"); // $ işaretini kullanmazsal o zaman içerisine int değerini yazamazdık.
 
 
 
             //                                             ---------------------- UYGULAMA --------------------------
 
-            Console.Write("Dikdörtgen Kısa: ");
-            int dikdörtgen_kısa = int.Parse(Console.ReadLine());
+            // Console.Write("Dikdörtgen Kısa: ");
+            // int dikdörtgen_kısa = int.Parse(Console.ReadLine());
 
-            Console.Write("Dikdörtgen Uzun: ");
-            int dikdörtgen_uzun = int.Parse(Console.ReadLine());
+            // Console.Write("Dikdörtgen Uzun: ");
+            // int dikdörtgen_uzun = int.Parse(Console.ReadLine());
 
 
-            // Bu şekilde var değeri de yazılabilir yani bir sayı değeri belirtmeye gerek yoktır.
-            var alan = dikdörtgen_kısa * dikdörtgen_uzun;
-            var cevre = (dikdörtgen_kısa + dikdörtgen_uzun) * 2;
+            // // Bu şekilde var değeri de yazılabilir yani bir sayı değeri belirtmeye gerek yoktır.
+            // var alan = dikdörtgen_kısa * dikdörtgen_uzun;
+            // var cevre = (dikdörtgen_kısa + dikdörtgen_uzun) * 2;
 
-            // Console.WriteLine("alan: " + alan + " " + "çevre: " + cevre);
-            Console.WriteLine($"alan: {alan} çevre: {cevre}");
+            // // Console.WriteLine("alan: " + alan + " " + "çevre: " + cevre);
+            // Console.WriteLine($"alan: {alan} çevre: {cevre}");
 
             //                                          ----------------------- DATE-TİME -------------------------
 
             // Date-Time Bilgisi ile zaman bilgileri alınır Now ile şuan ki zaman bilgisi alınır.
 
-            string[] aylar = { "Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık" };
-            DateTime simdi = DateTime.Now;
+            // string[] aylar = { "Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık" };
+            // DateTime simdi = DateTime.Now;
 
-            Console.WriteLine(simdi);
-            Console.WriteLine(simdi.Year); // Şimdiki zamanın yıl bilgisini getirir.
-            Console.WriteLine(simdi.Month);
-            Console.WriteLine(aylar[simdi.Month - 1]);
-            Console.WriteLine(simdi.Day);
-            Console.WriteLine(simdi.DayOfWeek); // Getirdiği günden hangi gün olduğunu belirtir
-            Console.WriteLine(simdi.Hour);
-            Console.WriteLine(simdi.Minute);
-            Console.WriteLine(simdi.Second);
+            // Console.WriteLine(simdi);
+            // Console.WriteLine(simdi.Year); // Şimdiki zamanın yıl bilgisini getirir.
+            // Console.WriteLine(simdi.Month);
+            // Console.WriteLine(aylar[simdi.Month - 1]);
+            // Console.WriteLine(simdi.Day);
+            // Console.WriteLine(simdi.DayOfWeek); // Getirdiği günden hangi gün olduğunu belirtir
+            // Console.WriteLine(simdi.Hour);
+            // Console.WriteLine(simdi.Minute);
+            // Console.WriteLine(simdi.Second);
 
-            DateTime dt = new DateTime(2025, 3, 8, 12, 46, 21);
+            // DateTime dt = new DateTime(2025, 3, 8, 12, 46, 21);
 
-            // Add Metodu ile ekleme ve çıkarma yapabiliriz zamana
-            DateTime dt1 = dt.AddDays(2);
-            DateTime dt2 = dt.AddYears(1);
-            DateTime dt3 = dt.AddHours(-5);
+            // // Add Metodu ile ekleme ve çıkarma yapabiliriz zamana
+            // DateTime dt1 = dt.AddDays(2);
+            // DateTime dt2 = dt.AddYears(1);
+            // DateTime dt3 = dt.AddHours(-5);
 
-            var fark = simdi - dt;
+            // var fark = simdi - dt;
 
-            // Total değerleri ile toplam günü saati yada başka bir değeri getirtebiliriz.
-            Console.WriteLine(fark.TotalDays);
-            Console.WriteLine(fark.TotalHours);
+            // // Total değerleri ile toplam günü saati yada başka bir değeri getirtebiliriz.
+            // Console.WriteLine(fark.TotalDays);
+            // Console.WriteLine(fark.TotalHours);
 
 
-            // Burada ToString'in içerisine yazdığımız değerler sayesinde istediğimiz tarih bilgisini getirtebiliriz.
-            Console.WriteLine(simdi);
-            Console.WriteLine(simdi.ToString("d"));
-            Console.WriteLine(simdi.ToString("D"));
-            Console.WriteLine(simdi.ToString("F"));
-            Console.WriteLine(simdi.ToString("M"));
-            Console.WriteLine(simdi.ToString("t"));
-            Console.WriteLine(simdi.ToString("T"));
-            Console.WriteLine(simdi.ToString("Y"));
+            // // Burada ToString'in içerisine yazdığımız değerler sayesinde istediğimiz tarih bilgisini getirtebiliriz.
+            // Console.WriteLine(simdi);
+            // Console.WriteLine(simdi.ToString("d"));
+            // Console.WriteLine(simdi.ToString("D"));
+            // Console.WriteLine(simdi.ToString("F"));
+            // Console.WriteLine(simdi.ToString("M"));
+            // Console.WriteLine(simdi.ToString("t"));
+            // Console.WriteLine(simdi.ToString("T"));
+            // Console.WriteLine(simdi.ToString("Y"));
 
-            Console.WriteLine(simdi.ToString("hh:mm:ss"));
+            // Console.WriteLine(simdi.ToString("hh:mm:ss"));
 
-            // Burada yazdığım değerler zamanın nasıl getiriliceğini burada belirtmiş oluruz.
-            Console.WriteLine(simdi.ToString("ddd MMM %d, yyyy"));
+            // // Burada yazdığım değerler zamanın nasıl getiriliceğini burada belirtmiş oluruz.
+            // Console.WriteLine(simdi.ToString("ddd MMM %d, yyyy"));
 
-            CultureInfo culture = new CultureInfo("en"); // Burada hangi dildi olucağını belirttik en yani ingilizce
-            Console.WriteLine(simdi.ToString("F", culture)); // ToString de ilk önce formatı belirtilir sonrasında o formatın uygulanacağı değer yazılır.
+            // CultureInfo culture = new CultureInfo("en"); // Burada hangi dildi olucağını belirttik en yani ingilizce
+            // Console.WriteLine(simdi.ToString("F", culture)); // ToString de ilk önce formatı belirtilir sonrasında o formatın uygulanacağı değer yazılır.
 
 
             //                                  -------------------- Aritmatik Operatörler -----------------------
@@ -171,25 +170,25 @@ namespace ConsoleApp
 
             // 2- Atama Operatörleri (=,+=,-=,*=,/=,%=)
 
-            int xa = 5, ys = 10, zs = 20;
-            double val;
+            // int xa = 5, ys = 10, zs = 20;
+            // double val;
 
-            // Buradaki += yada -= gibi değerler x değerinin üstüne 5 ekle demek yada 5 çıkar demektir geriye kalanlarda aynı mantıkdadır.
+            // // Buradaki += yada -= gibi değerler x değerinin üstüne 5 ekle demek yada 5 çıkar demektir geriye kalanlarda aynı mantıkdadır.
 
-            xa += 5;
-            xa -= 5;
-            xa *= 5;
-            xa /= 5;
-            xa %= 5;
-            val = Math.Pow(2, 5); // Buradaki Pow bilgisi üslü sayı yazmak için kullanılır yani aslında buradaki değer 2 üzeri 5 tir.
-            val = Math.Sqrt(25); // Sqrt içine girilen değerin karekökünü alır.
-            val = Math.Abs(-10); // Abs değeri negatif değeri pozitif değere döndürür.
-            val = Math.Round(4.5); // Sayıyı kendine en yakın sayıya yuvarlar
-            val = Math.Ceiling(5.5); // Her zaman yukarıya yuvarlama yapar 
-            val = Math.Floor(5.7); // Her zaman aşşağıya yuvarlama yapar.
+            // xa += 5;
+            // xa -= 5;
+            // xa *= 5;
+            // xa /= 5;
+            // xa %= 5;
+            // val = Math.Pow(2, 5); // Buradaki Pow bilgisi üslü sayı yazmak için kullanılır yani aslında buradaki değer 2 üzeri 5 tir.
+            // val = Math.Sqrt(25); // Sqrt içine girilen değerin karekökünü alır.
+            // val = Math.Abs(-10); // Abs değeri negatif değeri pozitif değere döndürür.
+            // val = Math.Round(4.5); // Sayıyı kendine en yakın sayıya yuvarlar
+            // val = Math.Ceiling(5.5); // Her zaman yukarıya yuvarlama yapar 
+            // val = Math.Floor(5.7); // Her zaman aşşağıya yuvarlama yapar.
 
-            Console.WriteLine($"x: {x} y: {ys} z: {z}");
-            Console.WriteLine(val);
+            // Console.WriteLine($"x: {x} y: {ys} z: {z}");
+            // Console.WriteLine(val);
 
             //                                             --------------- UYGULAMA ------------------
 
@@ -225,19 +224,19 @@ namespace ConsoleApp
 
             // 3) Karşılaştırma Operatörleri (==,!=,=>,=<,?:)
 
-            int aa = 5, bb = 5, cc = 10, dd = 4;
-            string username = "ÇınarÖzgürgil";
-            string password = "123456";
+            // int aa = 5, bb = 5, cc = 10, dd = 4;
+            // string username = "ÇınarÖzgürgil";
+            // string password = "123456";
 
-            var result = (a == b); // true
+            // var result = (a == b); // true
             //result = (a == c); // false değeri döner
             //result = (username == "Çnaz");
 
             // 4) Mantıksal Operatörler (&&, || , !)
 
-            int xx = 6;
-            int hak = 5;
-            char devami = 'e';
+            // int xx = 6;
+            // int hak = 5;
+            // char devami = 'e';
 
             // and (&&) [Girilen iki değerinde doğru ise doğru yanlış ise yanlış olması gerekir başka türlü olmaz]
             // true && true => true
@@ -352,6 +351,76 @@ namespace ConsoleApp
             // }
 
             // Console.WriteLine("döngü dışı");
+
+
+            //      ------------------------------------ Asal Sayı (Uygulama) ------------------------------------------
+
+
+            // Console.Write("Sayı: ");
+            // int say = int.Parse(Console.ReadLine());
+
+            // int[] sayılar = { 2, 3, 5, 7 };
+
+            // bool asalmi = true;
+
+            // foreach (int sayı in sayılar)
+            // {
+            //     if (say == sayı)
+            //     {
+            //         asalmi = true;
+            //         break;
+            //     }
+            //     else if (say % sayı == 0)
+            //     {
+            //         asalmi = false;
+            //         break;
+            //     }
+            // }
+
+            // if (asalmi)
+            // {
+            //     Console.WriteLine("Asaldır");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Asal değildir");
+            // }
+
+            //                                  ---------------------- SAYI TAHMİN UYGULAMASI ------------------------------
+
+
+            // Random random = new Random();
+
+            // int rastgele = random.Next(1, 10);
+
+            // for (int hakkı = 5; hakkı > 0; hakkı--)
+            // {
+            //     Console.Write("Sayı: ");
+            //     int sayı = int.Parse(Console.ReadLine());
+
+            //     if (sayı == rastgele)
+            //     {
+            //         Console.WriteLine("Tebrikler Bildiniz.");
+            //         break;
+            //     }
+
+            //     else if (sayı > rastgele)
+            //     {
+            //         Console.WriteLine("Daha küçük bir değer giriniz.");
+            //     }
+
+            //     else if (rastgele < sayı)
+            //     {
+            //         Console.WriteLine("Daha Büyük bir değer giriniz.");
+            //     }
+
+            //     else
+            //     {
+            //         Console.WriteLine("Maalesef Bilemediniz");
+            //     }
+
+            // }
+
 
         }
     }
