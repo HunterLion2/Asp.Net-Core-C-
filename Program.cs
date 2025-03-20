9d3fbb585b66e1431dba34f7e108037131d7c15e
Çınar Özgürgil <cozgurgil@gmail.com>
2025-03-20 22:54:06 +0300

    Hata Ve Hata Yönetimi

--- Program.cs
+++ Program.cs
@@ -1134,8 +1134,39 @@
 
 // ----------------------------------------------------------------------- Hata Ve Hata Yönetimi ------------------------------------------------------------------------
 
-    static void Main(string[] args) {
-        
+    class Product{
+        public string Name { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+           // Bu şekilde sistemdeki hataları bir yere yazmış olduk.
+
+           // Exception 
+           // Unhandled exception. System.FormatException
+           // Unhandled exception. System.DivideByZeroException
+           // Unhandled exception. System.IndexOutOfRangeException
+           // Unhandled exception. System.NullReferenceException
+
+           Console.Write("1. sayı: ");
+           int sayi1 =int.Parse(Console.ReadLine());
+
+           Console.Write("2. sayı: ");
+           int sayi2 =int.Parse(Console.ReadLine());
+
+           var sonuc = sayi1 / sayi2;
+
+           int[] sayilar = new int[2];
+           // sayilar[2] = 10;
+
+           Product p = null;
+           Console.WriteLine(p.Name);
+
+           // Exception Handling
+           
+        }
     }
 
 //         }
