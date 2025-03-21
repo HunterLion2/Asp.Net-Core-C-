7cb05e5eebc8d52e6a939da5e8787b8bf8fd933a
Çınar Özgürgil <cozgurgil@gmail.com>
2025-03-21 14:48:37 +0300

    Hata-Sınıfımız

--- Program.cs
+++ Program.cs
@@ -1256,7 +1256,46 @@
     //     }
     // }
 
-    
+// --------------------------------------------------------------- Exception(Kendi Hata Sınıfımızı Yazma) ------------------------------------------------------------------------------------
+
+
+
+    // Kendi hata sınıfımızı yazarken bu şekilde ana Exception'a bağlarız ve aldığımız mesajları oraya da göndeririz.
+    class LoginException: Exception
+    {
+        public LoginException(string message):base(message)
+        {
+            
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Login("sadikturan","123456777");
+                Console.WriteLine("login başarılı.");
+            }
+            catch (LoginException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        static void Login(string username, string password)
+        {
+            if (username.Contains(" "))
+                throw new LoginException("username boşluk içeremez.");
+
+            if (password.Length<8)
+                throw new LoginException("Parola min. 8 karakter olmalıdır.");
+        }
+    }
 
 
 //         }
